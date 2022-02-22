8126204687cb7b19cfb8218bd97a8ebe976d20e0
NibeshMali <nibeshmali25@gmail.com>
2022-02-21 19:15:32 -0800

    Adding file to repo

---
+++ src/main/java/com/seattle/seattle/SeattleApplication.java
@@ -0,0 +1,13 @@
+package com.seattle.seattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seattle/seattle/SeattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seattle.seattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
