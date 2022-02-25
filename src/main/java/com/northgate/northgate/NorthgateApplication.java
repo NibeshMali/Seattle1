dd0dae54804c188dd6b060a91dc1b94443796220
NibeshMali <nibeshmali25@gmail.com>
2022-02-24 16:11:16 -0800

    adding

---
+++ src/main/java/com/northgate/northgate/NorthgateApplication.java
@@ -0,0 +1,13 @@
+package com.northgate.northgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthgateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthgateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northgate/northgate/NorthgateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northgate.northgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthgateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
